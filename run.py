24c204b07917da0f8632a260b82948b4c08dc093
Joshua <joshuas@marketsa.co.za>
2021-09-08 09:53:27 +0200

    fixed variable naming

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 #string concatenation (aka how to put strings together)
 
-adj = input("Adjective: ")
+adjective = input("Adjective: ")
 verb1 = input("Verb: ")
 verb2 = input("Verb: ")
 famousPerson = input("Famous Person: ")
 
-sentence = f"Computer programming makes me so {adj}! It makes me so excited all the time because \
+sentence = f"Computer programming makes me so {adjective}! It makes me so excited all the time because \
 I love to {verb1}. Stay safe and {verb2} like you are {famousPerson}!"
     
 print(sentence)
